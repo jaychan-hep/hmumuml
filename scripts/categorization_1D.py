d6056ebbf644d53440f4b9c7648fe41a6f4aed84
Jay Chan <jay.chan@cern.ch>
2021-08-20 05:46:23 +0200

    allow significance from full-sim

--- scripts/categorization_1D.py
+++ scripts/categorization_1D.py
@@ -34,32 +34,39 @@ def getArgs():
     #parser.add_argument('--val', action = 'store_true', default = False, help = 'use validation samples for categroization')
     parser.add_argument('-t', '--transform', action = 'store_true', default = True, help = 'use the transform scores for categroization')
     parser.add_argument('--floatB', action = 'store_true', default = False, help = 'Floting last boundary')
+    parser.add_argument('-es', '--estimate', action = 'store', choices = ['fullSim', 'fullSimrw', 'data_sid'], default = 'data_sid', help = 'Method to estimate significance')
     parser.add_argument('-f', '--nfold', type = int, default = 1, help='number of folds.')
     parser.add_argument('-e', '--earlystop', type = int, default = -1, help='early stopping rounds.')
     return  parser.parse_args()
 
-def gettingsig(region, variable, boundaries, transform):
+def gettingsig(region, variable, boundaries, transform, estimate):
 
-    yields = pd.DataFrame({'sig': [0.]*len(boundaries[0]),
-                          'sig_err': [0.]*len(boundaries[0]),
-                          'bkg': [0.]*len(boundaries[0]),
-                          'bkg_err': [0.]*len(boundaries[0]),
-                          'VBF': [0.]*len(boundaries[0]),
-                          'VBF_err': [0.]*len(boundaries[0]),
-                          'ggF': [0.]*len(boundaries[0]),
-                          'ggF_err': [0.]*len(boundaries[0])})
+    nbin = len(boundaries[0])
 
-    for category in ['sig', 'bkg', 'VBF', 'ggF']:
+    yields = pd.DataFrame({'sig': [0.]*nbin,
+                          'sig_err': [0.]*nbin,
+                          'data_sid': [0.]*nbin,
+                          'data_sid_err': [0.]*nbin,
+                          'bkgmc_sid': [0.]*nbin,
+                          'bkgmc_sid_err': [0.]*nbin,
+                          'bkgmc_cen': [0.]*nbin,
+                          'bkgmc_cen_err': [0.]*nbin,
+                          'VBF': [0.]*nbin,
+                          'VBF_err': [0.]*nbin,
+                          'ggF': [0.]*nbin,
+                          'ggF_err': [0.]*nbin})
 
-        for data in tqdm(read_root(f'outputs/{region}/{category}.root', key='test', columns=[f"{variable}_score{'_t' if transform else ''}", 'm_mumu', 'weight', 'eventNumber'], chunksize=500000), desc=f'Loading {category}', bar_format='{desc}: {percentage:3.0f}%|{bar:20}{r_bar}'):
+    for category in ['sig', 'VBF', 'ggF', "data_sid", "bkgmc_sid", "bkgmc_cen"]:
+
+        for data in tqdm(read_root(f'outputs/{region}/{"bkgmc" if "bkgmc" in category else category}.root', key='test', columns=[f"{variable}_score{'_t' if transform else ''}", 'm_mumu', 'weight', 'eventNumber'], chunksize=500000), desc=f'Loading {category}', bar_format='{desc}: {percentage:3.0f}%|{bar:20}{r_bar}'):
     
-            if category in ['sig', 'VBF', 'ggF']:
+            if 'sid' in category:
+                data = data[(data.m_mumu >= 110) & (data.m_mumu <= 180) & ((data.m_mumu < 120) | (data.m_mumu > 130))]
+                data['w'] = data.weight
+
+            else:
                 data = data[(data.m_mumu >= 120) & (data.m_mumu <= 130)]
                 data['w'] = data.weight
-    
-            elif category == 'bkg':
-                data = data[(data.m_mumu >= 110) & (data.m_mumu <= 180) & ((data.m_mumu < 120) | (data.m_mumu > 130))]
-                data['w'] = data.weight * 0.2723
     
             for i in range(len(boundaries)):
     
@@ -72,6 +79,16 @@ def gettingsig(region, variable, boundaries, transform):
     
                     yields[category][j] += data_ss.w.sum()
                     yields[category+'_err'][j] = np.sqrt(yields[category+'_err'][j]**2 + (data_ss.w**2).sum())
+
+    if estimate == "data_sid":
+        yields['bkg'] = yields['data_sid']*0.2723
+        yields['bkg_err'] = yields['data_sid_err']*0.2723
+    elif estimate == "fullSimrw":
+        yields['bkg'] = yields['data_sid']*yields['bkgmc_cen']/yields['bkgmc_sid']
+        yields['bkg_err'] = yields['data_sid_err']*yields['bkgmc_cen']/yields['bkgmc_sid']
+    elif estimate == "fullSim":
+        yields['bkg'] = yields['bkgmc_cen']
+        yields['bkg_err'] = yields['bkgmc_cen_err']
 
     zs = calc_sig(yields.sig, yields.bkg, yields.sig_err, yields.bkg_err)
     yields['z'] = zs[0]
@@ -88,22 +105,41 @@ def gettingsig(region, variable, boundaries, transform):
 
     return z, abs(u)
 
-def categorizing(region, variable, sigs, bkgs, nscan, minN, transform, nbin, floatB, n_fold, fold, earlystop):
+def categorizing(region, variable, sigs, bkgs, nscan, minN, transform, nbin, floatB, n_fold, fold, earlystop, estimate):
 
     f_sig = TFile('outputs/%s/sig.root' % (region))
     t_sig = f_sig.Get('test')
  
-    f_bkg = TFile('outputs/%s/bkg.root' % (region))
-    t_bkg = f_bkg.Get('test')
+    if estimate in ["fullSim", "fullSimrw"]:
+        f_bkgmc = TFile('outputs/%s/bkgmc.root' % (region))
+        t_bkgmc = f_bkgmc.Get('test')
+    if estimate in ["fullSimrw", "data_sid"]:
+        f_data_sid = TFile('outputs/%s/data_sid.root' % (region))
+        t_data_sid = f_data_sid.Get('test')
 
     h_sig = TH1F('h_sig','h_sig',nscan,0,1)
     h_bkg = TH1F('h_bkg','h_bkg',nscan,0,1)
 
     t_sig.Draw(f"{variable}_score{'_t' if transform else ''}>>h_sig", "weight*%f*((m_mumu>=120&&m_mumu<=130)&&(eventNumber%%%d!=%d))"%(n_fold/(n_fold-1.) if n_fold != 1 else 1, n_fold, fold if n_fold != 1 else 1))
-    t_bkg.Draw(f"{variable}_score{'_t' if transform else ''}>>h_bkg", "weight*%f*(0.2723)*((m_mumu>=110&&m_mumu<=180)&&!(m_mumu>=120&&m_mumu<=130)&&(eventNumber%%%d!=%d))"%(n_fold/(n_fold-1.) if n_fold != 1 else 1, n_fold, fold if n_fold != 1 else 1))
 
-    print('INFO: scanning all of the possibilities...')
-    cgz = categorizer(h_sig, h_bkg)
+    # filling bkg histograms
+    if estimate in ["fullSim", "fullSimrw"]:
+        h_bkgmc_cen = TH1F('h_bkgmc_cen', 'h_bkgmc_cen', nscan, 0., 1.)
+        t_bkgmc.Draw(f"{variable}_score{'_t' if transform else ''}>>h_bkgmc_cen", "weight*%f*((m_mumu>=120&&m_mumu<=130)&&(eventNumber%%%d!=%d))"%(n_fold/(n_fold-1.) if n_fold != 1 else 1, n_fold, fold if n_fold != 1 else 1))
+    if estimate in ["fullSimrw"]:
+        h_bkgmc_sid = TH1F('h_bkgmc_sid', 'h_bkgmc_sid', nscan, 0., 1.)
+        t_bkgmc.Draw(f"{variable}_score{'_t' if transform else ''}>>h_bkgmc_sid", "weight*%f*((m_mumu>=110&&m_mumu<=180)&&!(m_mumu>=120&&m_mumu<=130)&&(eventNumber%%%d!=%d))"%(n_fold/(n_fold-1.) if n_fold != 1 else 1, n_fold, fold if n_fold != 1 else 1))
+    if estimate in ["fullSimrw", "data_sid"]:
+        h_data_sid = TH1F('h_data_sid', 'h_data_sid', nscan, 0., 1.)
+        t_data_sid.Draw(f"{variable}_score{'_t' if transform else ''}>>h_data_sid", "weight*%f*((m_mumu>=110&&m_mumu<=180)&&!(m_mumu>=120&&m_mumu<=130)&&(eventNumber%%%d!=%d))"%(n_fold/(n_fold-1.) if n_fold != 1 else 1, n_fold, fold if n_fold != 1 else 1))
+
+    if estimate == "data_sid":
+        h_data_sid.Scale(0.2723)
+        cgz = categorizer(h_sig, h_data_sid)
+    elif estimate == "fullSimrw":
+        cgz = categorizer(h_sig, h_data_sid, h_bkg_rw_num=h_bkgmc_cen, h_bkg_rw_den=h_bkgmc_sid)
+    elif estimate == "fullSim":
+        cgz = categorizer(h_sig, h_bkgmc_cen)
     #cgz.smooth(60, nscan)  #uncomment this line to fit a function to the BDT distribution. Usage: categorizer.smooth(left_bin_to_fit, right_bin_to_fit, SorB='S' (for signal) or 'B' (for bkg), function='Epoly2', printMessage=False (switch to "True" to print message))
     bmax, zmax = cgz.fit(1, nscan, nbin, minN=minN, floatB=floatB, earlystop=earlystop, pbar=True)
 
@@ -128,7 +164,7 @@ def main():
 
     sigs = ['ggF','VBF','VH','ttH']
 
-    bkgs = ['data_sid']
+    bkgs = ["Z", "ttbar", "diboson", "stop"]
 
     if args.floatB and args.nbin == 16:
         print('ERROR: With floatB option, the maximun nbin is 15!!')
@@ -147,12 +183,11 @@ def main():
             if os.path.isfile('outputs/%s/%s.root'% (region,sig)): siglist+=' outputs/%s/%s.root'% (region,sig)
         os.system("hadd -f outputs/%s/sig.root"%(region)+siglist)
 
-    #if not os.path.isfile('outputs/model_%s_%s/bkg.root' % (train_sig,region)):
     if not args.skip:
         bkglist=''
         for bkg in bkgs:
             if os.path.isfile('outputs/%s/%s.root'% (region,bkg)): bkglist+=' outputs/%s/%s.root'% (region,bkg)
-        os.system("hadd -f outputs/%s/bkg.root"%(region)+bkglist)
+        os.system("hadd -f outputs/%s/bkgmc.root"%(region)+bkglist)
 
 
     n_fold = args.nfold
@@ -160,7 +195,7 @@ def main():
     boundaries_values=[]
     smaxs = []
     for j in range(n_fold):
-        bound, bound_value, smax = categorizing(region, variable, sigs, bkgs, nscan, args.minN, args.transform, args.nbin if not args.floatB else args.nbin + 1, args.floatB, n_fold, j, args.earlystop)
+        bound, bound_value, smax = categorizing(region, variable, sigs, bkgs, nscan, args.minN, args.transform, args.nbin if not args.floatB else args.nbin + 1, args.floatB, n_fold, j, args.earlystop, estimate=args.estimate)
         boundaries.append(bound)
         boundaries_values.append(bound_value)
         smaxs.append(smax)
@@ -168,7 +203,7 @@ def main():
     smax = sum(smaxs)/n_fold
     print('Averaged significance: ', smax)
 
-    s, u = gettingsig(region, variable, boundaries_values, args.transform)
+    s, u = gettingsig(region, variable, boundaries_values, args.transform, estimate=args.estimate)
 
     outs={}
     outs['boundaries'] = boundaries
@@ -183,6 +218,7 @@ def main():
     outs['minN'] = args.minN
     outs['fine_tuned'] = False
     outs['variable'] = variable
+    outs['estimate'] = args.estimate
 
     if not os.path.isdir('significances/%s'%region):
         print(f'INFO: Creating output folder: "significances/{region}"')
