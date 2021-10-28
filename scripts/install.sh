3590a6fa0f31f0de1bbe920a95c99069d95cbd6f
Tuan Minh Pham <tuan.minh.pham@cern.ch>
2021-10-28 20:58:19 +0200

    Update scripts/install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
 # include ROOT
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.24.02/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+python3 -m venv hmumumlenv
 source hmumumlenv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
