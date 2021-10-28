ddf6aedecc00e2293a021682d4ceadb5f54b149f
Tuan Minh Pham <tuan.minh.pham@cern.ch>
2021-10-28 19:08:28 +0200

    Update scripts/install.sh, scripts/setup.sh files

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,25 +1,8 @@
-# create environment from the `environment.yml`
-# conda env create -f environment.yml
-# conda activate hmumuml
-
-# source /cvmfs/sft.cern.ch/lcg/releases/LCG_97apython3/ROOT/v6.20.06/x86_64-centos7-gcc8-opt/ROOT-env.sh
-python3 -m venv hmumumlenv
 # include ROOT
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.24.02/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 source hmumumlenv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-
-
-# create virtual environment
-#conda create -n hmumuml python=3.7 # --clone base
-#conda activate hmumuml
-
-# install packages
-#conda config --add channels conda-forge
-#conda config --add channels anaconda
-#conda install root root_numpy
-#conda install h5py scikit-learn matplotlib tabulate xgboost pandas root_pandas tqdm
 
 # setting python path
 export PATH="`pwd`/scripts:${PATH}"
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,9 +2,6 @@
 # include ROOT
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.24.02/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 source hmumumlenv/bin/activate
-#export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-#source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh
-# conda activate hmumumlenv
 
 export PATH="`pwd`:${PATH}"
 export PYTHONPATH="`pwd`:${PYTHONPATH}"
